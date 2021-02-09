43f88040d123b7721522ef68e0c473cb06990801
roach <giraftw2002@gmail.com>
2021-02-09 12:58:00 +0800

    finialize two wheels control

--- scripts/twist_to_motors.py
+++ scripts/twist_to_motors.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 """
    twist_to_motors - converts a twist message to motor commands.  Needed for navigation stack
-   
-   
-    Copyright (C) 2012 Jon Stephan. 
-     
+
+
+    Copyright (C) 2012 Jon Stephan.
+
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
@@ -21,7 +21,8 @@ import rospy
 # import roslib
 from std_msgs.msg import Float64
 # from rospy_tutorials.msg import Floats
-from geometry_msgs.msg import Twist 
+# from std_msgs.msg import Int32MultiArray
+from geometry_msgs.msg import Twist
 
 # mapping -1, 1 (m/s) to -200, 200
 from numpy import interp
@@ -38,55 +39,73 @@ class TwistToMotors(object):
         rospy.init_node("twist_to_motors")
         nodename = rospy.get_name()
         rospy.loginfo("%s started" % nodename)
-        
-        self.L = rospy.get_param("wheel_separation", 0.2)   
+
+        # what func to call when ctrl-c is pressed
+        rospy.on_shutdown(self.shutdown)
+
+        self.L = rospy.get_param("wheel_separation", 0.2)
         self.R = rospy.get_param("wheel_radius", 0.035)
         # 20ms
-        self.rate = rospy.get_param("rate", 50)        
-    
+        self.rate = rospy.get_param("rate", 50)
+
         # self.pub_lmotor = rospy.Publisher('lwheel_vtarget', Float64, queue_size=10)
-        self.pub_rmotor = rospy.Publisher('/single_joint_actuator/joint1_velocity_controller/command', Float64, queue_size=10)
+        # pub to h/w interface. the topic name must match with yaml and launch files.
+        self.pub_rmotor = rospy.Publisher(            
+            '/single_joint_actuator/joint1_velocity_controller/command', Float64, queue_size=10)
+        self.pub_lmotor = rospy.Publisher('/single_joint_actuator/lwheel/command', Float64, queue_size=10)
+
         # rospy.Subscriber('twist', Twist, self.twistCallback)
-        rospy.Subscriber('/cmd_vel', Twist, self.twistCallback)        
-          
+        rospy.Subscriber('/cmd_vel', Twist, self.twistCallback)
+
         self.timeout_ticks = rospy.get_param("~timeout_ticks", 2)
         self.left = self.left_mapped = 0
         self.right = self.right_mapped = 0
-        
+        self.motor_speed = {}
+
     #############################################################
     def spin(self):
     #############################################################
-    
+
         r = rospy.Rate(self.rate)
         idle = rospy.Rate(10)
         then = rospy.Time.now()
         self.ticks_since_target = self.timeout_ticks
-    
+
         ###### main loop ######
         while not rospy.is_shutdown():
-        
+
             while not rospy.is_shutdown() and self.ticks_since_target < self.timeout_ticks:
                 self.spinOnce()
                 r.sleep()
             idle.sleep()
-                
+
     #############################################################
     def spinOnce(self):
     #############################################################
-    
+
         # dx = (l + r) / 2
         # dr = (r - l) / w
-         
-        self.right = 1.0 * self.dx + self.dr * self.L / 2 
-        self.left = 1.0 * self.dx - self.dr * self.L / 2
-        #rospy.loginfo("publishing: (%d, %d)", self.left, self.right)
 
+        self.right = 1.0 * self.dx + self.dr * self.L / 2
+        self.left = 1.0 * self.dx - self.dr * self.L / 2
+        # rospy.loginfo("publishing: (%5.2f, %5.2f)", self.left, self.right)
+        
         # mapping makes tunning pid coefficents easier
         self.right_mapped = interp(self.right, [-1, 1], [-200, 200])
-        # self.v[1] = interp(self.left, [-1, 1], [-200, 200])
+        self.left_mapped  = interp(self.left, [-1, 1], [-200, 200])
 
-        #self.pub_lmotor.publish(self.left_mapped)
-        self.pub_rmotor.publish(self.right_mapped)
+        if(self.left_mapped < 0 and self.right_mapped > 0):
+            self.motor_speed = [49, self.right_mapped]
+        elif(self.left_mapped > 0 and self.right_mapped < 0):	  
+            self.motor_speed = [self.left_mapped, 49]
+        else:
+            self.motor_speed = [self.left_mapped, self.right_mapped]
+
+        self.pub_rmotor.publish(self.motor_speed[0])
+        self.pub_lmotor.publish(self.motor_speed[1])
+
+        # self.pub_lmotor.publish(self.left_mapped)
+        # self.pub_rmotor.publish(self.right_mapped)
             
         self.ticks_since_target += 1
 
@@ -99,6 +118,13 @@ class TwistToMotors(object):
         self.dr = msg.angular.z
         self.dy = msg.linear.y
     
+    def shutdown(self):        
+        rospy.loginfo("Stop roachBot")
+        self.motor_speed.data = {0}
+        self.pub_motors.publish(self.motor_speed)
+        # makes sure robot receive the stop cmd prior to shutting down
+        self.rospy.sleep(1)
+
 #############################################################
 #############################################################
 if __name__ == '__main__':
--- src/single_joint_hardware_interface.cpp
+++ src/single_joint_hardware_interface.cpp
@@ -4,9 +4,10 @@
 ROBOTHardwareInterface::ROBOTHardwareInterface(ros::NodeHandle& nh) : nh_(nh) {
     init();
     controller_manager_.reset(new controller_manager::ControllerManager(this, nh_));
-    loop_hz_=5;
+    loop_hz_=5;	
     ros::Duration update_freq = ros::Duration(1.0/loop_hz_);
 	
+	// pub = nh_.advertise<rospy_tutorials::Floats>("/joints_to_aurdino",10);
 	pub = nh_.advertise<rospy_tutorials::Floats>("/joints_to_aurdino",10);
 	client = nh_.serviceClient<diff_drive::joint_state>("/read_joint_state");
 	
@@ -17,103 +18,37 @@ ROBOTHardwareInterface::~ROBOTHardwareInterface() {
 }
 
 void ROBOTHardwareInterface::init() {
-// Initialization of the robot's resources (joints, sensors, actuators) and
-		// interfaces can be done here or inside init().
-		// E.g. parse the URDF for joint names & interfaces, then initialize them
-		/* Create a JointStateHandle for each joint and register them with the JointStateInterface.
-		** connect and register the joint state interface
-		*/
-		hardware_interface::JointStateHandle state_handle_a("joint1", &pos[0], &vel[0], &eff[0]);
-		joint_state_interface_.registerHandle(state_handle_a);
+	// Initialization of the robot's resources (joints, sensors, actuators) and
+	// interfaces can be done here or inside init().
+	// E.g. parse the URDF for joint names & interfaces, then initialize them
+	/* Create a JointStateHandle for each joint and register them with the JointStateInterface.
+	** connect and register the joint state interface
+	*/
+	hardware_interface::JointStateHandle state_handle_a("joint1", &pos[0], &vel[0], &eff[0]);
+	jnt_state_interface_.registerHandle(state_handle_a);
 
-		// left joint
-		hardware_interface::JointStateHandle state_handle_b("joint2", &pos[1], &vel[1], &eff[1]);
-		joint_state_interface_.registerHandle(state_handle_b);
+	// left joint
+	hardware_interface::JointStateHandle state_handle_b("joint2", &pos[1], &vel[1], &eff[1]);
+	jnt_state_interface_.registerHandle(state_handle_b);
 
-		// Register the JointStateInterface containing the read only joints
-		// with this robot's hardware_interface::RobotHW.
-		registerInterface(&joint_state_interface_);
+	// Register the JointStateInterface containing the read only joints
+	// with this robot's hardware_interface::RobotHW.
+	registerInterface(&jnt_state_interface_);
 
-		// connect and register the joint position interface
-		// Create a JointHandle (read and write) for each controllable joint
-		// using the read-only joint handles within the JointStateInterface and 
-		// register them with the JointEffortInterface.
-		hardware_interface::JointHandle effort_handle_a(joint_state_interface_.getHandle("joint1"), &cmd[0]);
-        effort_joint_interface_.registerHandle(effort_handle_a);
+	// connect and register the joint position interface
+	// Create a JointHandle (read and write) for each controllable joint
+	// using the read-only joint handles within the JointStateInterface and 
+	// register them with the JointEffortInterface.
+	hardware_interface::JointHandle effort_handle_a(jnt_state_interface_.getHandle("joint1"), &cmd[0]);
+	effort_joint_interface_.registerHandle(effort_handle_a);
+	
+	hardware_interface::JointHandle effort_handle_b(jnt_state_interface_.getHandle("joint2"), &cmd[1]);
+	effort_joint_interface_.registerHandle(effort_handle_b);
 
-		hardware_interface::JointHandle effort_handle_b(joint_state_interface_.getHandle("joint2"), &cmd[1]);
-		effort_joint_interface_.registerHandle(effort_handle_b);
-
-		// Register the JointEffortInterface containing the read/write joints
-		// with this robot's hardware_interface::RobotHW.
-		registerInterface(&effort_joint_interface_);
-
-		//pub = n.advertise<rospy_tutorials::Floats>("/joints_to_aurdino", 10);
-		//client = n.serviceClient<diff_drive::joint_state>("/read_joint_state");
+	// Register the JointEffortInterface containing the read/write joints
+	// with this robot's hardware_interface::RobotHW.
+	registerInterface(&effort_joint_interface_);
 }
-
-/*
-void ROBOTHardwareInterface::init() {
-    
-    
-	joint_name_="joint1";
-    
-// Create joint state interface
-    hardware_interface::JointStateHandle jointStateHandle(joint_name_, &pos[0], &vel[0], &eff[0]);
-    joint_state_interface_.registerHandle(jointStateHandle);
-
-// Create position joint interface
-    //hardware_interface::JointHandle jointPositionHandle(jointStateHandle, &joint_position_command_);
-    //position_joint_interface_.registerHandle(jointPositionHandle);
-    
-// Create velocity joint interface
-	//hardware_interface::JointHandle jointVelocityHandle(jointStateHandle, &joint_velocity_command_);
-    //effort_joint_interface_.registerHandle(jointVelocityHandle);
-    
-// Create effort joint interface
-    hardware_interface::JointHandle jointEffortHandle(jointStateHandle, &cmd[0]);
-	effort_joint_interface_.registerHandle(jointEffortHandle);
-	
-// Create Joint Limit interface   
-    // joint_limits_interface::JointLimits limits;
-    // joint_limits_interface::getJointLimits("joint1", nh_, limits);
-    
-	// joint_limits_interface::EffortJointSaturationHandle jointLimitsHandle(jointEffortHandle, limits);
-	// effortJointSaturationInterface.registerHandle(jointLimitsHandle);
-	
------------
-If you have more joints then,
-    joint_name_= "joint2"
-    
-// Create joint state interface
-    hardware_interface::JointStateHandle jointStateHandle2(joint_name_, &joint_position_2, &joint_velocity_2, &joint_effort_2);
-    joint_state_interface_.registerHandle(jointStateHandle);
-//create the position/velocity/effort interface according to your actuator 
-    hardware_interface::JointHandle jointPositionHandle2(jointStateHandle2, &joint_position_command_2);
-    position_joint_interface_.registerHandle(jointPositionHandle2);
-    
-    hardware_interface::JointHandle jointVelocityHandle2(jointStateHandle2, &joint_velocity_command_2);
-    effort_joint_interface_.registerHandle(jointVelocityHandle2);
-    
-    hardware_interface::JointHandle jointEffortHandle2(jointStateHandle2, &joint_effort_command_2);
-	effort_joint_interface_.registerHandle(jointEffortHandle2);
-	
-//create joint limit interface.
-    joint_limits_interface::getJointLimits("joint2", nh_, limits);
-	joint_limits_interface::EffortJointSaturationHandle jointLimitsHandle2(jointEffortHandle2, limits);
-	effortJointSaturationInterface.registerHandle(jointLimitsHandle2);
-	
-	Repeat same for other joints
-----------------
-	
-
-// Register all joints interfaces    
-    registerInterface(&joint_state_interface_);
-    // registerInterface(&position_joint_interface_);
-    registerInterface(&effort_joint_interface_);
-    // registerInterface(&effortJointSaturationInterface);
-}
-*/
 
 void ROBOTHardwareInterface::update(const ros::TimerEvent& e) {
     elapsed_time_ = ros::Duration(e.current_real - e.last_real);
@@ -139,11 +74,9 @@ if more than one joint,
 	}
 	else
 	{
-	    pos[0] = 0;
-	    vel[0] = 0;
-	}
-        
-
+	    pos[2] = {0};
+	    vel[2] = {0};		
+	}      
 }
 
 void ROBOTHardwareInterface::write(ros::Duration elapsed_time) {
@@ -151,18 +84,17 @@ void ROBOTHardwareInterface::write(ros::Duration elapsed_time) {
     // effortJointSaturationInterface.enforceLimits(elapsed_time);    
 	joints_pub.data.clear();
 	joints_pub.data.push_back(cmd[0]);
-	
+	joints_pub.data.push_back(cmd[1]);
 /*
 if more than one joint,
     publish values for joint_effort_command_2,......
 */	
 	
-	ROS_INFO("PWM Cmd: %.2f", cmd[0]);
+	ROS_INFO("PWM Cmd: [%5.2f, %5.2f]", cmd[0], cmd[1]);
 	pub.publish(joints_pub);
+	
 		
 }
-
-
 
 int main(int argc, char** argv)
 {
